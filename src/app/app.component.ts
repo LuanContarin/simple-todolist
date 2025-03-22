77d1bd55a25df599ea70f64655a8105a3fa8749d
LuanContarin <luan.silvacontarin@gmail.com>
2025-03-22 20:51:40 -0300

    Added tailwindcss and created todo list logic+styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { Task } from '../models/task';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [CommonModule, FormsModule],
 })
 export class AppComponent {
-  title = 'simple-todolist';
+  tasks: Task[] = [];
+  new_task?: string;
+
+  addTask() {
+    if (!this.new_task) return;
+
+    this.tasks.push({ description: this.new_task, completed: false });
+    this.new_task = '';
+  }
+
+  toggleTask(index: number) {
+    this.tasks[index].completed = !this.tasks[index].completed;
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
 }
